125162a7e0cd0749a0dc78226e9db031d39013dd
lchenri <lucasbonicardoso@outlook.com>
2024-02-17 15:45:14 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/lucas/desafiotodo_list/DesafioToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package lucas.desafiotodo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
